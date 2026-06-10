a3a7467371009b11a4f0a51c460e6b45b8f661d3
Kunal Sawardekar <kunalsawardekar07@gmail.com>
2026-06-10 12:48:50 +0530

    added Swagger UI

--- services/alert/src/main/java/com/logverine/incident/alert/controller/AlertController.java
+++ services/alert/src/main/java/com/logverine/incident/alert/controller/AlertController.java
@@ -3,12 +3,16 @@ package com.logverine.incident.alert.controller;
 import com.logverine.incident.alert.dto.AlertResponse;
 import com.logverine.incident.alert.dto.CreateAlertRequest;
 import com.logverine.incident.alert.service.AlertService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/alerts")
+@Tag(name = "Alert API", description = "Alert Management APIs")
 public class AlertController {
 
     private final AlertService alertService;
@@ -19,6 +23,7 @@ public class AlertController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create Alert")
     public AlertResponse createAlert(
             @Valid @RequestBody CreateAlertRequest request
     ) {
